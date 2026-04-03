75cfc4458e0a7eacc76c97d14c02b2d70783b99d
Tywanek <Tywanek@users.noreply.github.com>
2026-04-03 13:37:41 +0200

    FetchTasksUseCaseTest

--- app/src/main/java/com/radlab/todolist/domain/usecase/AddTaskUseCase.kt
+++ app/src/main/java/com/radlab/todolist/domain/usecase/AddTaskUseCase.kt
@@ -2,10 +2,11 @@ package com.radlab.todolist.domain.usecase
 
 import com.radlab.todolist.domain.repository.TaskRepository
 
-class AddTaskUseCase(private val repository: TaskRepository) {
-    suspend operator fun invoke(message: String) {
-        if (message.isNotBlank()) {
-            repository.addTask(message)
+class AddTaskUseCase(private val repository: TaskRepository)
+    : UseCase<String, Unit> {
+    override suspend fun invoke(input: String) {
+        if(input.isNotBlank()){
+            repository.addTask(input)
         }
     }
 }
